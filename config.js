aed11686a259d322e9bdeb08ba3bc5c54fd8886d
Asma <fathimasma26@gmail.com>
2024-10-30 12:44:05 +0530

    first

---
+++ config.js
@@ -0,0 +1,4 @@
+const myHeaders = new Headers();
+myHeaders.append("x-apihub-key", "eKA8UvVEitctRbZDrcIlpwriOX53rCthq0ZmKZyVP1agO-xkvg");
+myHeaders.append("x-apihub-host", "Open-Trivia-API.allthingsdev.co");
+myHeaders.append("x-apihub-endpoint", "e39df3a7-4e1c-40c4-8449-b7ff3ed7b16c");
